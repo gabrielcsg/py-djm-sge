9fffbf96ea04ec604f23025950429f6aa8f6fa0b
Gabriel Castro <gabrielcssg@gmail.com>
2025-01-17 15:40:27 -0300

    feat: crud categories

--- categories/admin.py
+++ categories/admin.py
@@ -5,4 +5,5 @@ from .models import Category
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'created_at', 'updated_at')
+    list_display = ('name', 'description', 'created_at',)
+    search_fields = ('name',)
