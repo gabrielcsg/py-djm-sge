79d3084ddec9e229b2b3f74a242b09116b7e0625
Gabriel Castro <gabrielcssg@gmail.com>
2025-01-20 14:33:23 -0300

    feat: login and logout

--- app/urls.py
+++ app/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
 
 from . import views
@@ -6,6 +7,9 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
     path('', views.home, name='home'),
     path('', include('brands.urls')),
