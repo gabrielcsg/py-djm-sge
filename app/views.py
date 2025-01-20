d084c4bdaa0d2ae1fa7d04c244f2b02da41c9a92
Gabriel Castro <gabrielcssg@gmail.com>
2025-01-20 11:20:15 -0300

    feat: products by categories and brands charts

--- app/views.py
+++ app/views.py
@@ -9,10 +9,14 @@ def home(request):
     sales_metrics = metrics.get_sales_metrics()
     daily_sales_data = metrics.get_daily_sales_data()
     daily_sales_quantity_data = metrics.get_daily_sales_quantity_data()
+    product_count_by_category = metrics.get_graphic_product_category_metric()
+    product_count_by_brand = metrics.get_graphic_product_brand_metric()
     context = {
         'product_metrics': product_metrics,
         'sales_metrics': sales_metrics,
         'daily_sales_data': json.dumps(daily_sales_data),
         'daily_sales_quantity_data': json.dumps(daily_sales_quantity_data),
+        'product_count_by_category': json.dumps(product_count_by_category),
+        'product_count_by_brand': json.dumps(product_count_by_brand),
     }
     return render(request, 'home.html', context=context)
